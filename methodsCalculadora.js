b2e18d1fb0cdc603c6d228319250db98de7ec9bc
isaquemc2001 <isaquebrushwoods@gmail.com>
2022-12-09 17:12:37 -0300

    atualiacao botoes

--- methodsCalculadora.js
+++ methodsCalculadora.js
@@ -1,5 +1,28 @@
 const vm = new Vue({
     el: '#app',
-    data: {},
-    methods: {}
+    data: {
+        // numeros
+        n1: 1,
+        n2: 2,
+        n3: 3,
+        n4: 4,
+        n5: 5,
+        n6: 6,
+        n7: 7,
+        n8: 8,
+        n9: 9,
+
+        // operadores
+        adicao: "+",
+        subtracao: "-",
+        multiplicacao: "*",
+        divisao: "/",
+        igualdade: "="
+
+    },
+    methods: {
+        soma(){
+            this.n1
+        }
+    }
 })
